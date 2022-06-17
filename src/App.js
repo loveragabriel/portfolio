6e4a5eb5ec9d9ef4ffd76a851385ef293757b47d
loveragabriel <loveragabriel20@gmail.com>
2022-06-17 15:53:28 -0300

    working on aboutMe page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import React from 'react';
 /* Pages*/
 import Home from './Components/Home';
 import ProjectSection from './Components/ProjectSection';
+import AboutMe from './Components/AboutMe';
 
 function App() {
   return (
     <div>
       <Home/>
       <ProjectSection/>
+      <AboutMe/>
     </div>
   );
 }
