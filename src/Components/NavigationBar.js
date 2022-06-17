70c1121d61350fb7da2feaf07ae904eda8da0754
loveragabriel <loveragabriel20@gmail.com>
2022-06-17 13:43:17 -0300

    Icon added to Navigation Bar

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import StyledComponents from './StyledComponents/StyledComponents'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+
 
 export default function NavigationBar() {
   return (
     <nav>
         <div className='logo'>
+            <FontAwesomeIcon icon={faUser} />
         </div>
-            <p>ICON</p>
         <ul className='nav-link'>
             <li><a href=''>Home</a></li>
             <li><a href=''>Contact</a></li>
