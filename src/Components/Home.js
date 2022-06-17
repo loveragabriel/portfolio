1ba929d3517c185f595b52047c281f310f631115
loveragabriel <loveragabriel20@gmail.com>
2022-06-17 14:12:59 -0300

    Banner img and text completed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <div className='Home-page'>
         <NavigationBar/>
+        <div className='ppl-Img'>
+            <section className='banner-section'>
+            <h2>Gabriel Lovera</h2>
+            <p>Webdesign</p> 
+            </section>  
+        </div>
     </div>
 
   )
