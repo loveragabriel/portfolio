4cf1a1a40184da6e6973c1d043d60b43e72473a6
loveragabriel <loveragabriel20@gmail.com>
2022-06-19 19:33:03 -0300

    text added react project section

--- src/Components/ProjectSection.js
+++ src/Components/ProjectSection.js
@@ -9,6 +9,7 @@ export default function ProjectSection() {
                 <a href='https://learingreact.netlify.app/' alt="react app" target="blank"><img src='https://cdn.pixabay.com/photo/2015/12/11/09/30/mobile-phone-1087845_960_720.jpg'></img></a>
                 <p>Web App</p>
                 <p>HTML-CSS-JAVASCRIPT-REACT</p>
+                <p>Under Construction</p>
             </div>
             <div className='Card-item'>
                 <a href='https://loveragabriel.github.io/uncaffeperfavore/' alt="Web page bootstrap" target="_blank"><img src='https://cdn.pixabay.com/photo/2016/03/26/23/23/starbucks-1281880_960_720.jpg'></img></a>
